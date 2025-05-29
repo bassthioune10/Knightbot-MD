a51542a7db6e7031a1a48c126eb337ed1fea8ca4
bassthioune10 <157892138+bassthioune10@users.noreply.github.com>
2025-05-29 20:12:25 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight Bot',
   author: 'Bot‎',
-  botName: "Knight Bot",
-  botOwner: 'Knight Bot', // Your name
-  ownerNumber: '221776284197', //Set your number here without + symbol, just add country code & number without any space
+  botName: "BASS Hacker Bot",
+  botOwner: 'Bass Hacker', // Your name
+  ownerNumber: '221789547996', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
