a7f042e890f008da0cb3b9c726d24d6ab0c364e1
bassthioune10 <157892138+bassthioune10@users.noreply.github.com>
2025-05-31 22:16:55 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Bot‎',
   botName: "BASS Hacker Bot",
   botOwner: 'Bass Hacker', // Your name
-  ownerNumber: '221775307638', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '221789547996', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
